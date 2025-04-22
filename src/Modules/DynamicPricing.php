3d81bf7c79511ba2f24a9f27fd6e100e45d6cd7b
Cherminator38 <florian.garoby@gmail.com>
2025-04-22 08:21:50 +0200

    Add files via upload

--- src/Modules/DynamicPricing.php
+++ src/Modules/DynamicPricing.php
@@ -1,8 +1,54 @@
 <?php
 namespace PD\Modules;
 
+use WC_Product;
+
 class DynamicPricing {
     public static function init() {
-        // TODO: init DynamicPricing
+        // Hooks WooCommerce pour prix dynamiques
+        add_filter('woocommerce_product_get_price', [__CLASS__, 'apply_dynamic_price'], 10, 2);
+        add_filter('woocommerce_product_get_regular_price', [__CLASS__, 'apply_dynamic_price'], 10, 2);
+        add_filter('woocommerce_variation_get_price', [__CLASS__, 'apply_dynamic_price'], 10, 2);
+        add_filter('woocommerce_get_price_html', [__CLASS__, 'filter_price_html'], 10, 2);
+    }
+
+    public static function apply_dynamic_price($price, $product) {
+        $pid = $product->get_id();
+        if (! metadata_exists('post', $pid, '_lead_reference')) {
+            return $price;
+        }
+        // Calcul du temps écoulé en minutes
+        $elapsed = max(0, round((current_time('timestamp') - get_post_time('U', false, $pid)) / 60));
+        // Chargement des paliers depuis les meta
+        $tiers = [];
+        foreach (get_post_meta($pid) as $key => $vals) {
+            if (strpos($key, '_delai_palier_') === 0) {
+                $i = intval(str_replace('_delai_palier_', '', $key));
+                $tiers[$i] = [
+                    'delai' => intval($vals[0]),
+                    'prix'  => floatval(get_post_meta($pid, '_prix_palier_' . $i, true)),
+                ];
+            }
+        }
+        if (empty($tiers)) {
+            return $price;
+        }
+        ksort($tiers);
+        $current = $price;
+        foreach ($tiers as $tier) {
+            if ($elapsed >= $tier['delai']) {
+                $current = $tier['prix'];
+            } else {
+                break;
+            }
+        }
+        return $current;
+    }
+
+    public static function filter_price_html($html, $product) {
+        if (metadata_exists('post', $product->get_id(), '_lead_reference')) {
+            return wc_price($product->get_price());
+        }
+        return $html;
     }
 }
